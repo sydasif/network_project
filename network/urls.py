8e9656ef23e1e56c67613219851b0eaa9edb7f48
Syed Asif <sydasif78@gmail.com>
2025-05-20 11:20:51 +0500

    refectored UI

--- network/urls.py
+++ network/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import task_view
 
 urlpatterns = [
-    path("", task_view, name="task_view"),  # Your main view
+    path("", task_view, name="task_form"),  # Unified form endpoint
+    path("home/", task_view, name="home"),  # Dashboard landing page
 ]
