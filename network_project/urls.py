754e0f101cb34dae55fb924f18eef3e7e4c2977c
Syed Asif <sydasif78@gmail.com>
2025-05-22 09:45:49 +0500

    improved UI and add logo

--- network_project/urls.py
+++ network_project/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("network.urls")),  # Make this default app route
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
